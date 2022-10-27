399a77aaf6e998a600038a0b421ac0d5766799e9
김성길 <82089918+SungGil-5125@users.noreply.github.com>
2022-10-27 09:12:32 +0900

    :tada: :: project setting

---
+++ src/test/java/com/project/findme/FindmeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.findme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindmeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
