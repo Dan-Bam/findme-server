e974c3306d3ba418d9effd1fb0da18b043d9daae
김성길 <82089918+SungGil-5125@users.noreply.github.com>
2022-10-31 00:06:48 +0900

    :sparkles: :: @EnableConfigurationProperties 어노테이션 추가

---
+++ src/main/java/com/project/findme/global/exception/handler/GlobalExceptionHandler.java
@@ -0,0 +1,32 @@
+package com.project.findme.global.exception.handler;
+
+import com.project.findme.domain.user.exception.PasswordNotMatchException;
+import com.project.findme.domain.user.exception.UserNotFoundException;
+import com.project.findme.global.exception.ErrorResponse;
+import com.project.findme.domain.user.exception.DuplicateIdException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(DuplicateIdException.class)
+    public ResponseEntity<ErrorResponse> DuplicateIdException(DuplicateIdException e) {
+        ErrorResponse errorResponse = new ErrorResponse(e.getErrorCode());
+        return new ResponseEntity<>(errorResponse, HttpStatus.valueOf(e.getErrorCode().getStatus()));
+    }
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<ErrorResponse> UserNotFoundException(UserNotFoundException e) {
+        ErrorResponse errorResponse = new ErrorResponse(e.getErrorCode());
+        return new ResponseEntity<>(errorResponse, HttpStatus.valueOf(e.getErrorCode().getStatus()));
+    }
+
+    @ExceptionHandler(PasswordNotMatchException.class)
+    public ResponseEntity<ErrorResponse> PasswordNotMatchException(PasswordNotMatchException e) {
+        ErrorResponse errorResponse = new ErrorResponse(e.getErrorCode());
+        return new ResponseEntity<>(errorResponse, HttpStatus.valueOf(e.getErrorCode().getStatus()));
+    }
+}
