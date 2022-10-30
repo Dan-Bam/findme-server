acd22bb05cf556b1e67eef937bc6676153089682
김성길 <82089918+SungGil-5125@users.noreply.github.com>
2022-10-31 00:10:45 +0900

    :sparkles: :: jwt 요청 필터 추가

---
+++ src/main/java/com/project/findme/global/security/jwt/JwtRequestFilter.java
@@ -0,0 +1,56 @@
+package com.project.findme.global.security.jwt;
+
+import io.jsonwebtoken.JwtException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JwtRequestFilter extends OncePerRequestFilter {
+
+    private final JwtTokenProvider jwtTokenProvider;
+    private final UserDetailsService userDetailsService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String accessToken = request.getHeader("Authorization");
+
+        if (accessToken != null) {
+            String id = accessTokenExtractEmail(accessToken);
+            registerUserInfoSecurityContext(id, request);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    public String accessTokenExtractEmail(String accessToken) {
+        try {
+            return jwtTokenProvider.getUserId(accessToken);
+        } catch (JwtException e) {
+            throw new RuntimeException();
+        }
+    }
+
+    public void registerUserInfoSecurityContext(String id, HttpServletRequest request) {
+        try {
+            UserDetails userDetails = userDetailsService.loadUserByUsername(id);
+            UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+            usernamePasswordAuthenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+            SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+        } catch (NullPointerException e) {
+            throw new RuntimeException();
+        }
+    }
+}
