e89b6a2a0351e42453a20adcb21c2519610f9759
김성길 <82089918+SungGil-5125@users.noreply.github.com>
2022-10-31 21:14:53 +0900

    :sparkles: :: 로그아웃 기능 추가

--- src/main/java/com/project/findme/domain/user/service/Impl/UserServiceImpl.java
+++ src/main/java/com/project/findme/domain/user/service/Impl/UserServiceImpl.java
@@ -4,10 +4,10 @@ import com.project.findme.domain.user.entity.User;
 import com.project.findme.domain.user.exception.PasswordNotMatchException;
 import com.project.findme.domain.user.exception.RefreshTokenExpiredException;
 import com.project.findme.domain.user.exception.UserNotFoundException;
-import com.project.findme.domain.user.presentation.dto.request.SignInDto;
-import com.project.findme.domain.user.presentation.dto.request.SignUpDto;
-import com.project.findme.domain.user.presentation.dto.response.ReissueTokenResponse;
-import com.project.findme.domain.user.presentation.dto.response.SignInResponseDto;
+import com.project.findme.domain.user.presentation.dto.SignInDtoRequest;
+import com.project.findme.domain.user.presentation.dto.SignUpRequest;
+import com.project.findme.domain.user.presentation.dto.ReissueTokenResponse;
+import com.project.findme.domain.user.presentation.dto.SignInResponse;
 import com.project.findme.domain.user.repository.UserRepository;
 import com.project.findme.domain.user.service.UserService;
 import com.project.findme.domain.user.exception.DuplicateIdException;
@@ -29,16 +29,16 @@ public class UserServiceImpl implements UserService {
 
     @Override
     @Transactional(rollbackFor = Exception.class)
-    public void signUp(SignUpDto signUpDto) {
+    public void signUp(SignUpRequest signUpRequest) {
 
-        userRepository.findById(signUpDto.getId()).orElseThrow(() -> new DuplicateIdException("중복된 아이디 입니다."));
-        userRepository.save(signUpDto.toEntity(passwordEncoder.encode(signUpDto.getPassword())));
+        userRepository.findById(signUpRequest.getId()).orElseThrow(() -> new DuplicateIdException("중복된 아이디 입니다."));
+        userRepository.save(signUpRequest.toEntity(passwordEncoder.encode(signUpRequest.getPassword())));
 
     }
 
     @Override
     @Transactional(rollbackFor = Exception.class)
-    public SignInResponseDto signIn(SignInDto signInDto) {
+    public SignInResponse signIn(SignInDtoRequest signInDto) {
 
         User user = userRepository.findById(signInDto.getId()).orElseThrow(() -> new UserNotFoundException("아이디를 찾을 수 없습니다다."));
         if (!passwordEncoder.matches(signInDto.getPassword(), user.getPassword())) {
@@ -49,7 +49,7 @@ public class UserServiceImpl implements UserService {
 
         user.updateRefreshToken(refreshToken);
 
-        return SignInResponseDto.builder()
+        return SignInResponse.builder()
                 .accessToken(accessToken)
                 .refreshToken(refreshToken)
                 .build();
--- src/main/java/com/project/findme/domain/user/service/UserService.java
+++ src/main/java/com/project/findme/domain/user/service/UserService.java
@@ -1,14 +1,14 @@
 package com.project.findme.domain.user.service;
 
-import com.project.findme.domain.user.presentation.dto.request.SignInDto;
-import com.project.findme.domain.user.presentation.dto.request.SignUpDto;
-import com.project.findme.domain.user.presentation.dto.response.ReissueTokenResponse;
-import com.project.findme.domain.user.presentation.dto.response.SignInResponseDto;
+import com.project.findme.domain.user.presentation.dto.SignInDtoRequest;
+import com.project.findme.domain.user.presentation.dto.SignUpRequest;
+import com.project.findme.domain.user.presentation.dto.ReissueTokenResponse;
+import com.project.findme.domain.user.presentation.dto.SignInResponse;
 
 public interface UserService {
 
-    void signUp(SignUpDto signUpDto);
-    SignInResponseDto signIn(SignInDto signInDto);
+    void signUp(SignUpRequest signUpRequest);
+    SignInResponse signIn(SignInDtoRequest signInDto);
     ReissueTokenResponse reissueToken(String token);
     void logout();
 }
