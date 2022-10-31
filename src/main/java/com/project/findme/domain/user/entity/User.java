59f69ca11e7040a101056ec5477ff6ac9614e762
김성길 <82089918+SungGil-5125@users.noreply.github.com>
2022-11-01 08:13:40 +0900

    :sparkles: :: 기본생성자 accessLevel 수정

--- src/main/java/com/project/findme/domain/user/entity/User.java
+++ src/main/java/com/project/findme/domain/user/entity/User.java
@@ -1,10 +1,7 @@
 package com.project.findme.domain.user.entity;
 
 import com.project.findme.domain.user.type.Role;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.persistence.*;
 import java.util.ArrayList;
@@ -14,7 +11,7 @@ import java.util.List;
 @Builder
 @Getter
 @AllArgsConstructor
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class User {
 
     @Id
