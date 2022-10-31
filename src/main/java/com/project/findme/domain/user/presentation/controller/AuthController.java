85ce13db70c5d89ffdfd4f04f8f68d2b1d1f4a7f
김성길 <82089918+SungGil-5125@users.noreply.github.com>
2022-10-31 21:13:54 +0900

    :sparkles: :: 로그인 회원가입 컨트롤러 작성

--- src/main/java/com/project/findme/domain/user/presentation/controller/AuthController.java
+++ src/main/java/com/project/findme/domain/user/presentation/controller/AuthController.java
@@ -1,9 +1,9 @@
 package com.project.findme.domain.user.presentation.controller;
 
-import com.project.findme.domain.user.presentation.dto.request.SignInDto;
-import com.project.findme.domain.user.presentation.dto.request.SignUpDto;
-import com.project.findme.domain.user.presentation.dto.response.ReissueTokenResponse;
-import com.project.findme.domain.user.presentation.dto.response.SignInResponseDto;
+import com.project.findme.domain.user.presentation.dto.SignInDtoRequest;
+import com.project.findme.domain.user.presentation.dto.SignUpRequest;
+import com.project.findme.domain.user.presentation.dto.ReissueTokenResponse;
+import com.project.findme.domain.user.presentation.dto.SignInResponse;
 import com.project.findme.domain.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -20,15 +20,15 @@ public class AuthController {
     private final UserService userService;
 
     @PostMapping("signup")
-    public ResponseEntity<Void> signUp(@RequestBody @Valid SignUpDto signUpDto) {
-        userService.signUp(signUpDto);
+    public ResponseEntity<Void> signUp(@RequestBody @Valid SignUpRequest signUpRequest) {
+        userService.signUp(signUpRequest);
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
     @PostMapping("signin")
-    public ResponseEntity<SignInResponseDto> signIn(@RequestBody @Valid SignInDto signInDto) {
-        SignInResponseDto result = userService.signIn(signInDto);
-        return new ResponseEntity<>(result, HttpStatus.OK);
+    public ResponseEntity<SignInResponse> signIn(@RequestBody @Valid SignInDtoRequest signInDto) {
+        SignInResponse token = userService.signIn(signInDto);
+        return new ResponseEntity<>(token, HttpStatus.OK);
     }
 
     @PatchMapping("refresh")
