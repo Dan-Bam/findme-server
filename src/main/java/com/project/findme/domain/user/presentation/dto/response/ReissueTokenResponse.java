c3a78c108da9062ca2b54fd748533ed0a12bb96a
김성길 <82089918+SungGil-5125@users.noreply.github.com>
2022-10-31 10:18:59 +0900

    :sparkles: :: new refreshToken response

---
+++ src/main/java/com/project/findme/domain/user/presentation/dto/response/ReissueTokenResponse.java
@@ -0,0 +1,14 @@
+package com.project.findme.domain.user.presentation.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter @Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ReissueTokenResponse {
+
+    private String newRefreshToken;
+}
