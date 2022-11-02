4a3f3b0a8c891ee7a69d20b93d58642f36fb9379
김성길 <82089918+SungGil-5125@users.noreply.github.com>
2022-11-02 15:09:04 +0900

    :sparkles: :: Message RedisHash 생성

---
+++ src/main/java/com/project/findme/domain/message/entity/MessageAuth.java
@@ -0,0 +1,21 @@
+package com.project.findme.domain.message.entity;
+
+import lombok.*;
+import org.hibernate.validator.constraints.Length;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@RedisHash(value = "messageAuth", timeToLive = 60 * 10)
+public class MessageAuth {
+
+    @Id
+    private String phoneNumber;
+
+    @Length(max = 4)
+    private Integer authKey;
+
+}
