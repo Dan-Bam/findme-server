067675d9991b4ad5771d9973b2619bb5f427729e
김성길 <82089918+SungGil-5125@users.noreply.github.com>
2022-11-03 23:09:43 +0900

    :sparkles: :: 인증 요청 permitAll 추가

--- src/main/java/com/project/findme/global/security/SecurityConfig.java
+++ src/main/java/com/project/findme/global/security/SecurityConfig.java
@@ -1,14 +1,22 @@
 package com.project.findme.global.security;
 
+import com.project.findme.global.security.jwt.JwtRequestFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtRequestFilter jwtRequestFilter;
 
     @Bean
     protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -17,6 +25,26 @@ public class SecurityConfig {
                 .and()
                 .csrf().disable()
                 .httpBasic().disable();
+        http
+                .authorizeRequests()
+                .antMatchers(HttpMethod.POST,
+                        "/auth/signup",
+                        "/auth/signin",
+                        "/message"
+                ).permitAll()
+                .antMatchers(HttpMethod.GET,
+                        "/message"
+                ).permitAll()
+                .antMatchers(HttpMethod.PATCH,
+                        "refresh"
+                ).permitAll()
+                .anyRequest().authenticated();
+
+        http
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http
+                .addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
