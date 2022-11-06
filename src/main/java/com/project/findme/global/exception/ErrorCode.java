7a4f5ad8698cd249980b4af12f914c9e87c8dede
김성길 <82089918+SungGil-5125@users.noreply.github.com>
2022-11-06 23:43:47 +0900

    :sparkles: :: TokenErrorCode status 403으로 통합

--- src/main/java/com/project/findme/global/exception/ErrorCode.java
+++ src/main/java/com/project/findme/global/exception/ErrorCode.java
@@ -9,17 +9,16 @@ public enum ErrorCode {
 
 
     // USER
-    DUPLICATE_ID_EXCEPTION("중복된 이메일입니다.", 400),
+    DUPLICATE_ID_EXCEPTION("중복된 이메일입니다.", 409),
     ID_NOT_FOUND("아이디를 찾을 수 없습니다.", 404),
     USER_NOT_FOUND("사용자를 찾을 수 없습니다", 404),
     PASSWORD_NOT_MATCH("비밀번호가 일치하지 않습니다.", 400),
 
 
     // TOKEN
-    REFRESH_TOKEN_EXPIRED("refreshToken이 만료되었습니다.", 400),
+    REFRESH_TOKEN_EXPIRED("refreshToken이 만료되었습니다.", 403),
     INVALID_TOKEN("유효하지 않은 토근입니다.", 403),
-    EXPIRATION_TOKEN("만료된 토큰입니다.", 400),
-    REFRESH_TOKEN_EXPIRATION("만료된 refreshToken 입니다.", 400),
+    EXPIRATION_TOKEN("만료된 토큰입니다.", 403),
 
 
     // MESSAGE
