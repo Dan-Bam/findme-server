62efa12a45a4aba43cccef222debc1874181480c
김성길 <82089918+SungGil-5125@users.noreply.github.com>
2022-11-08 23:29:23 +0900

    :sparkles: :: message endPoint 수정

--- src/main/java/com/project/findme/domain/message/presentation/controller/AuthMessageController.java
+++ src/main/java/com/project/findme/domain/message/presentation/controller/AuthMessageController.java
@@ -6,7 +6,10 @@ import com.project.findme.domain.message.service.MessageService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 
@@ -17,13 +20,14 @@ public class AuthMessageController {
 
     private final MessageService messageService;
 
-    @PostMapping("/send/message")
+    @PostMapping("/send")
     public ResponseEntity<Void> sendMessage(@RequestBody @Valid PhoneNumberRequest phoneNumberRequest) {
         messageService.sendMessage(phoneNumberRequest);
         return new ResponseEntity<>(HttpStatus.OK);
+
     }
 
-    @PostMapping("/check/message")
+    @PostMapping("/check")
     public ResponseEntity<Void> checkAuthKey(@RequestBody @Valid CheckAuthKeyRequest checkAuthKeyRequest) {
         messageService.checkAuthKey(checkAuthKeyRequest);
         return new ResponseEntity<>(HttpStatus.OK);
