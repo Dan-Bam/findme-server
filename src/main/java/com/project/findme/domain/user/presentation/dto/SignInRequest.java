008325549ac920e17c78feca9cfd84117e329d20
김성길 <82089918+SungGil-5125@users.noreply.github.com>
2022-11-09 13:59:55 +0900

    :sparkles: :: 비밀번호 제약조건 추가

--- src/main/java/com/project/findme/domain/user/presentation/dto/SignInRequest.java
+++ src/main/java/com/project/findme/domain/user/presentation/dto/SignInRequest.java
@@ -5,7 +5,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.*;
 
 @Getter
 @Builder
@@ -13,10 +13,11 @@ import javax.validation.constraints.NotBlank;
 @NoArgsConstructor
 public class SignInRequest {
 
-
     @NotBlank(message = "아이디가 입력되지 않았습니다")
     private String id;
 
+    @Pattern(regexp = "[0-9a-zA-Z]{0,20}", message = "영어와 숫자가 한글자라도 들어가야 합니다")
+    @Size(min = 5, max = 20, message = "비밀번호는 5~20자리여야 합니다")
     @NotBlank(message = "비밀번호가 입력되지 않았습니다")
     private String password;
 
--- src/main/java/com/project/findme/domain/user/presentation/dto/SignUpRequest.java
+++ src/main/java/com/project/findme/domain/user/presentation/dto/SignUpRequest.java
@@ -5,6 +5,8 @@ import com.project.findme.domain.user.type.Role;
 import lombok.*;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 import java.util.Collections;
 
 @Getter
@@ -16,6 +18,8 @@ public class SignUpRequest {
     @NotBlank(message = "아이디가 입력되지 않았습니다")
     private String id;
 
+    @Pattern(regexp = "[0-9a-zA-Z]{0,20}", message = "영어와 숫자가 한글자라도 들어가야 합니다")
+    @Size(min = 5, max = 20, message = "비밀번호는 5~20자리여야 합니다")
     @NotBlank(message = "비밀번호가 입력되지 않았습니다")
     private String password;
 
